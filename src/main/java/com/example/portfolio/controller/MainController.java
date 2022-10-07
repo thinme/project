d0691f07b3360d8714be94dbf2d54410655b098c
thinme <thinme12@naver.com>
2022-10-07 17:27:41 +0900

    2022-10-07 소스변경

--- src/main/java/com/example/portfolio/controller/MainController.java
+++ src/main/java/com/example/portfolio/controller/MainController.java
@@ -2,11 +2,16 @@ package com.example.portfolio.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
 
+    @GetMapping("/index")
+    public String index(){
+        return "/index";
+    }
 
 }
